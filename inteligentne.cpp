682e50a57f9ba92cf6eb8e420937e4c9e6137599
Blazej Leman <blazejleman789@gmail.com>
2023-11-01 17:48:29 +0100

    add calculateProduct()

--- inteligentne.cpp
+++ inteligentne.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
+#include <memory>
 
-// TODO: Implement calculateProduct()
-// It should take 2 int values and return their product as a shared_ptr.
-// Additionally, check how many owners are there.
+std::shared_ptr<int> calculateProduct(int num, int num2) {
+    return std::make_shared<int>(num*num2);
+}
 
 int main() {
     auto number = calculateProduct(10, 20);
